f17a1871c468b4cc4c8964fc42abcf1dcb9d644f
Ricard <ricard.aparicio@inqbarna.com>
2017-10-19 18:12:14 +0200

    * initial commit

---
+++ authenticator/src/main/java/com/bancsabadell/authsdk/ViewModel.kt
@@ -0,0 +1,8 @@
+package com.bancsabadell.authsdk
+
+/**
+ * Created by Kame on 19/10/2017.
+ */
+class ViewModel {
+
+}
---
+++ authenticator/src/main/java/com/bancsabadell/authsdk/WebViewActivity.kt
@@ -0,0 +1,13 @@
+package com.bancsabadell.authsdk
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Kame on 19/10/2017.
+ */
+class WebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
